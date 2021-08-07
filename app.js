78b32a93c4ea057f07bd8611be038ab77a39619f
erinpikachu <e.foster945@gmail.com>
2021-08-07 18:38:45 -0400

    Adding Counter to  Player Component

--- app.js
+++ app.js
@@ -10,13 +10,20 @@ const Player = () => {
     return (
         <div className="player">
             <span className="player-name">Erin</span>
-            <div className="counter">
-                <button className="counter-action decrement"> - </button>
-                <span className="counter-score">69</span>
-                <button className="counter-action increment"> + </button>
-            </div>
+            
+            <Counter />
         </div>
-    )
+    );
+}
+
+const Counter = () => {
+    return (
+        <div className="counter">
+            <button className="counter-action decrement"> - </button>
+            <span className="counter-score">69</span>
+            <button className="counter-action increment"> + </button>
+        </div>
+    );
 }
 
 ReactDOM.render(
