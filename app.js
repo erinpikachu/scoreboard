3ed5ecda01084b82ea9be7bab8a2b975037b1d09
erinpikachu <e.foster945@gmail.com>
2021-08-07 18:34:25 -0400

    Setting Up Player Component

--- app.js
+++ app.js
@@ -6,7 +6,20 @@ const Header = () => {
     </header>
 )};
 
+const Player = () => {
+    return (
+        <div className="player">
+            <span className="player-name">Erin</span>
+            <div className="counter">
+                <button className="counter-action decrement"> - </button>
+                <span className="counter-score">69</span>
+                <button className="counter-action increment"> + </button>
+            </div>
+        </div>
+    )
+}
+
 ReactDOM.render(
-    <Header />,
+    <Player />,
     document.getElementById('root')
 );
