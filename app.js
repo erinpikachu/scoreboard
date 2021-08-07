80951796f6b3e4dde296b7abd700b9ca61eee6fa
erinpikachu <e.foster945@gmail.com>
2021-08-07 19:36:06 -0400

    Setting Up Player Props

--- app.js
+++ app.js
@@ -1,43 +1,65 @@
-const Header = () => {
+const players = [
+    {
+        name: "Guil",
+        score: 50
+      },
+      {
+        name: "Treasure",
+        score: 85
+      },
+      {
+        name: "Ashley",
+        score: 95
+      },
+      {
+        name: "James",
+        score: 80
+      }
+];
+
+const Header = ( props ) => {
     return (
     <header>
-        <h1>Scoreboard</h1>
-        <span className="stats">Players: 1</span>
+        <h1>{props.title}</h1>
+        <span className="stats">Players: {props.totalPlayers}</span>
     </header>
 )};
 
-const Player = () => {
+const Player = ( props ) => {
     return (
         <div className="player">
-            <span className="player-name">Erin</span>
+            <span className="player-name">{props.name}</span>
 
-            <Counter />
+            <Counter score={props.score} />
         </div>
     );
 }
 
-const Counter = () => {
+const Counter = ( props ) => {
     return (
         <div className="counter">
             <button className="counter-action decrement"> - </button>
-            <span className="counter-score">69</span>
+            <span className="counter-score">{ props.score }</span>
             <button className="counter-action increment"> + </button>
         </div>
     );
 }
 
-const App = () => {
+const App = ( props ) => {
     return (
-        <div className="scoreboard">
-            <Header />
+        <main className="scoreboard">
+            <Header title="scoreboard" totalPlayers={props.initialPlayers.length} />
 
             {/* Players list */}
-            <Player />
-        </div>
+            {props.initialPlayers.map( player => 
+                <Player key={player.name} name={player.name} score={player.score} />
+            )}
+            
+        </main>
     )
 }
 
 ReactDOM.render(
-    <App />,
+    <App initialPlayers={players} />,
     document.getElementById('root')
 );
