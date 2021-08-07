7bcf1ad0e4fc3dadaa51d717a6d3e2cadc35f99b
erinpikachu <e.foster945@gmail.com>
2021-08-07 16:34:33 -0400

    Switching componet to Arroe Function

--- app.js
+++ app.js
@@ -1,11 +1,10 @@
-function Header() {
+const Header = () => {
     return (
-        <header>
-            <h1>Scoreboard</h1>
-            <span className="stats">Players: 1</span>
-        </header>
-    );
-}
+    <header>
+        <h1>Scoreboard</h1>
+        <span className="stats">Players: 1</span>
+    </header>
+)};
 
 ReactDOM.render(
     <Header />,
